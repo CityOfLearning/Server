c9ae6382659397c7bfaca371df67cd265437d219
Dominic Amato <dominic.a.amato@gmail.com>
2016-06-29 13:03:50 -0500

    fix metadata injection

--- ServerMod.java
+++ ServerMod.java
@@ -20,8 +20,8 @@ public class ServerMod {
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
 	public static Proxy proxy;
 
-	@Mod.Metadata(Reference.MOD_ID)
-	public ModMetadata metadata;
+//	@Mod.Metadata(Reference.MOD_ID)
+//	public ModMetadata metadata;
 
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
@@ -30,7 +30,8 @@ public class ServerMod {
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		metadata = MetaData.init(metadata);
+//		metadata = MetaData.init(metadata);
+		MetaData.init(event.getModMetadata());
 
 		PacketDispatcher.registerPackets();
 	}
