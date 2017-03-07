1ac998f8a34d346452a04353907ad4fca7d4b41e
Dominic Amato <dominic.a.amato@gmail.com>
2017-03-07 11:05:04 -0600

    update network packets for proximity block

--- network/messages/MessageProximityBlockUpdate.java
+++ network/messages/MessageProximityBlockUpdate.java
@@ -2,11 +2,13 @@ package com.dyn.server.network.messages;
 
 import com.dyn.fixins.blocks.redstone.proximity.ProximityBlockTileEntity;
 import com.dyn.server.ServerMod;
+import com.forgeessentials.commons.EnumMobType;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -21,6 +23,7 @@ public class MessageProximityBlockUpdate implements IMessage {
 				TileEntity tileEntity = world.getTileEntity(message.getPos());
 				if (tileEntity instanceof ProximityBlockTileEntity) {
 					((ProximityBlockTileEntity) tileEntity).setCorners(message.getCorner1(), message.getCorner2());
+					((ProximityBlockTileEntity) tileEntity).setValidMob(EnumMobType.fromName(message.getMobType()));
 					((ProximityBlockTileEntity) tileEntity).markForUpdate();
 				}
 			});
@@ -31,14 +34,16 @@ public class MessageProximityBlockUpdate implements IMessage {
 	private BlockPos pos;
 	private BlockPos corner1;
 	private BlockPos corner2;
+	private String mobType;
 
 	public MessageProximityBlockUpdate() {
 	}
 
-	public MessageProximityBlockUpdate(BlockPos pos, BlockPos corner1, BlockPos corner2) {
+	public MessageProximityBlockUpdate(BlockPos pos, BlockPos corner1, BlockPos corner2, String mobType) {
 		this.pos = pos;
 		this.corner1 = corner1;
 		this.corner2 = corner2;
+		this.mobType = mobType;
 	}
 
 	@Override
@@ -46,6 +51,7 @@ public class MessageProximityBlockUpdate implements IMessage {
 		pos = BlockPos.fromLong(buf.readLong());
 		corner1 = BlockPos.fromLong(buf.readLong());
 		corner2 = BlockPos.fromLong(buf.readLong());
+		mobType = ByteBufUtils.readUTF8String(buf);
 	}
 
 	public BlockPos getCorner1() {
@@ -65,5 +71,10 @@ public class MessageProximityBlockUpdate implements IMessage {
 		buf.writeLong(pos.toLong());
 		buf.writeLong(corner1.toLong());
 		buf.writeLong(corner2.toLong());
+		ByteBufUtils.writeUTF8String(buf, mobType);
+	}
+
+	public String getMobType() {
+		return mobType;
 	}
 }
